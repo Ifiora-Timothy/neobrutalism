d80254c8b700b0d66b442b7e7e3669a8c30943a1
Timothy <athanasiuscopy@gmail.com>
2024-08-24 18:12:56 +0100

    minor changes

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,12 +1,16 @@
 import { NextRequest, NextResponse } from "next/server";
 import { AllShirts } from "../../data";
+import { log } from "console";
 
 export const dynamic = "force-static";
 
 export async function GET(request: NextRequest) {
   //const searchParams = request.nextUrl.searchParams;
   //const query = searchParams.get("query");
-  const data = AllShirts;
+  console.log({ entered: "true" });
 
-  return NextResponse.json(AllShirts);
+  const data = AllShirts;
+  console.log({ data });
+
+  return NextResponse.json(data);
 }
